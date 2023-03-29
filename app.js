16edab82d49da9f9c47af455328388370dd5cc9b
Jar'Zeno <jarzenojarrett@gmail.com>
2023-03-29 11:21:57 -0700

    Set up mongoosedb connection

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(DB_URL);
+};
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
